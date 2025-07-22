41ab6976ced04c3841c8ca026d2859ffc88ed71f
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-07-22 13:43:21 +0530

    Create MainLayout.jsx

---
+++ src/Layout/MainLayout.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom"
+import Footer from "../Components/Footer/Footer"
+import Header from "../Components/Header/Header"
+
+const MainLayout = () => {
+  return (
+    <div  className="w-full h-screen">
+              <Header/>
+              <Outlet/>
+         <Footer/>
+      
+    </div>
+  )
+}
+
+export default MainLayout
