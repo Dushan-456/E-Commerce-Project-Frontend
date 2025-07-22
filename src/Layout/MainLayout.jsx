6dc0dd07feb7a4dd381c8dd0001f7ea8e9d63194
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-07-23 00:26:58 +0530

    Update MainLayout.jsx

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,16 +1,15 @@
-import { Outlet } from "react-router-dom"
-import Footer from "../Components/Footer/Footer"
-import Header from "../Components/Header/Header"
+import { Outlet } from "react-router-dom";
+import Footer from "../Components/Footer/Footer";
+import Header from "../Components/Header/Header";
 
 const MainLayout = () => {
-  return (
-    <div  className="w-full h-screen">
-              <Header/>
-              <Outlet/>
-         <Footer/>
-      
-    </div>
-  )
-}
+   return (
+      <div className="w-full h-screen">
+         <Header />
+         <Outlet />
+         <Footer />
+      </div>
+   );
+};
 
-export default MainLayout
+export default MainLayout;
