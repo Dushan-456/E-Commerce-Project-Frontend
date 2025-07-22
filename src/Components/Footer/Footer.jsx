0333e9a4c46cd82190e23e33231899e5f45566b7
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-07-23 00:26:43 +0530

    Update Footer.jsx

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,51 +1,96 @@
-
 const Footer = () => {
-  return (
-    <footer className="flex justify-between bg-fuchsia-500 w-full h-4/12">
-      <div className="column1">
-        <ol>
-          <li>List Items</li>
-          <li>List Items</li>
-          <li>List Items</li>
-          <li>List Items</li>
-          <li>List Items</li>
-          <li>List Items</li>
-        </ol>
-      </div>
-      <div className="column2">
-        <ol>
-          <li>List Items</li>
-          <li>List Items</li>
-          <li>List Items</li>
-          <li>List Items</li>
-          <li>List Items</li>
-          <li>List Items</li>
-        </ol>
-      </div>
-      <div className="column3">
-        <ol>
-          <li>List Items</li>
-          <li>List Items</li>
-          <li>List Items</li>
-          <li>List Items</li>
-          <li>List Items</li>
-          <li>List Items</li>
-        </ol>
-      </div>
-      <div className="column4">
-        <ol>
-          <li>List Items</li>
-          <li>List Items</li>
-          <li>List Items</li>
-          <li>List Items</li>
-          <li>List Items</li>
-          <li>List Items</li>
-        </ol>
-      </div>
+   return (
+      <footer className="flex justify-between bg-fuchsia-500 w-full h-4/12">
+         <div className="column1">
+            <ol>
+               <a href="">
+                  <li>List Items</li>
+               </a>
+               <a href="">
+                  <li>List Items</li>
+               </a>
+               <a href="">
+                  <li>List Items</li>
+               </a>
+               <a href="">
+                  <li>List Items</li>
+               </a>
+               <a href="">
+                  <li>List Items</li>
+               </a>
+               <a href="">
+                  <li>List Items</li>
+               </a>
+            </ol>
+         </div>
+         <div className="column2">
+            <ol>
+               <a href="">
+                  <li>List Items</li>
+               </a>
+               <a href="">
+                  <li>List Items</li>
+               </a>
+               <a href="">
+                  <li>List Items</li>
+               </a>
+               <a href="">
+                  <li>List Items</li>
+               </a>
+               <a href="">
+                  <li>List Items</li>
+               </a>
+               <a href="">
+                  <li>List Items</li>
+               </a>
+            </ol>
+         </div>
+         <div className="column3">
+            <ol>
+               <a href="">
+                  <li>List Items</li>
+               </a>
+               <a href="">
+                  <li>List Items</li>
+               </a>
+               <a href="">
+                  <li>List Items</li>
+               </a>
+               <a href="">
+                  <li>List Items</li>
+               </a>
+               <a href="">
+                  <li>List Items</li>
+               </a>
+               <a href="">
+                  <li>List Items</li>
+               </a>
+            </ol>
+         </div>
+         <div className="column4">
+            <ol>
+               <a href="">
+                  <li>List Items</li>
+               </a>
+               <a href="">
+                  <li>List Items</li>
+               </a>
+               <a href="">
+                  <li>List Items</li>
+               </a>
+               <a href="">
+                  <li>List Items</li>
+               </a>
+               <a href="">
+                  <li>List Items</li>
+               </a>
+               <a href="">
+                  <li>List Items</li>
+               </a>
+            </ol>
+         </div>
+      </footer>
+   );
+};
 
-
-    </footer>
-  )
-}
-
-export default Footer
+export default Footer;
