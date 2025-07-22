e3b9d61ac4bf62f8033c085971ba7485044ceea5
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-07-22 13:43:14 +0530

    Update Footer.jsx

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,50 @@
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <footer className="flex justify-between bg-fuchsia-500 w-full h-4/12">
+      <div className="column1">
+        <ol>
+          <li>List Items</li>
+          <li>List Items</li>
+          <li>List Items</li>
+          <li>List Items</li>
+          <li>List Items</li>
+          <li>List Items</li>
+        </ol>
+      </div>
+      <div className="column2">
+        <ol>
+          <li>List Items</li>
+          <li>List Items</li>
+          <li>List Items</li>
+          <li>List Items</li>
+          <li>List Items</li>
+          <li>List Items</li>
+        </ol>
+      </div>
+      <div className="column3">
+        <ol>
+          <li>List Items</li>
+          <li>List Items</li>
+          <li>List Items</li>
+          <li>List Items</li>
+          <li>List Items</li>
+          <li>List Items</li>
+        </ol>
+      </div>
+      <div className="column4">
+        <ol>
+          <li>List Items</li>
+          <li>List Items</li>
+          <li>List Items</li>
+          <li>List Items</li>
+          <li>List Items</li>
+          <li>List Items</li>
+        </ol>
+      </div>
+
+
+    </footer>
   )
 }
 
