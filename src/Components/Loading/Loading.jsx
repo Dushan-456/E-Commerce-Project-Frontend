ab84f3266526495edb5372369b0f5abff5122751
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-07-23 13:33:36 +0530

    Create Loading.jsx

---
+++ src/Components/Loading/Loading.jsx
@@ -0,0 +1,13 @@
+import './Loading.css'
+
+const Loading = () => {
+  return (
+    <div className='fixed top-0 left-0 gap-3 w-screen h-screen z-[9999] flex flex-col items-center justify-center bg-white/80 backdrop-blur-sm'>
+      <div className="loading-blob"></div>
+      <p>Loading..</p>
+      <div class="progress"></div>
+    </div>
+  )
+}
+
+export default Loading
