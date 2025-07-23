169d6eca3eabd32ae451d87b525db95785280c11
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-07-23 16:29:13 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,16 +1,29 @@
-import React, { useState } from 'react'
+import  { useEffect, useState } from 'react'
 import Ads from '../Components/ads/ads'
 import Products from '../Components/Products/Products'
 import Loading from '../Components/Loading/Loading'
+import CategoryItemsRound from '../Components/Categories/CategoryItemsRound'
 
 const Home = () => {
-    const [loading,setLoading]= useState(false)
+    const [loading,setLoading]= useState(true)
+
+
+    useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false)
+    }, 2000) // 2 seconds fake loading
+
+    return () => clearTimeout(timer) // cleanup
+  }, [])
+
+
     if(loading) return <Loading/>
     
   return (
     <div>
       <Ads/>
       <div className='w-7xl m-auto'>
+        <CategoryItemsRound/>
       <Products topic='Trending Products' rows={1} slidesPerView={5}/>
 
       </div>
