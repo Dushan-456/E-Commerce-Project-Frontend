4cfe2a5cc970429b1966ab0e139c8c5247ee0701
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-07-23 16:29:04 +0530

    Create ProductItem.jsx

---
+++ src/Components/Products/ProductItem.jsx
@@ -0,0 +1,41 @@
+import { Link } from "react-router-dom";
+import "./ProductItem.css";
+import Rating from "@mui/material/Rating";
+
+const ProductItem = ({   ProductID,ProductImgURL,   ProductTitle,   Price,   Discount,   ratings,   itemSold,}) => {
+  return (
+<Link to={`/product/${ProductID}`}>
+       <div className="product-card">
+      <img className="product-img" src={ProductImgURL} alt={ProductTitle}  style={{
+    width: '250px',
+    height: '250px',
+    objectFit: 'cover',
+    borderRadius: '8px',
+  }}/>
+      <p className="product-title w-[250px] overflow-hidden text-ellipsis whitespace-nowrap">{ProductTitle}</p>
+      <p className="price">
+         <span className="currency">LKR </span>
+         {(Price * (100 - Discount)) / 100}.00{" "}
+         <span className="old-price">{Price}.00</span>
+         <span className="discount"> -{Discount}%</span>
+      </p>
+
+      <div>
+         <Rating
+            name="half-rating-read"
+            defaultValue={ratings}
+            precision={0.5}
+            readOnly
+            size="small"
+         />
+         <span className="rate">{ratings}</span>
+         <span className="sold"> | {itemSold}+ sold</span>
+      </div>
+   </div>
+</Link>
+  )
+}
+
+export default ProductItem
+
+
