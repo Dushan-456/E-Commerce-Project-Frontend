b32e4bc4b984055dfa328cb839f7a04633dd6793
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-07-23 13:33:41 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Ads from '../Components/ads/ads'
 import Products from '../Components/Products/Products'
+import Loading from '../Components/Loading/Loading'
 
 const Home = () => {
+    const [loading,setLoading]= useState(false)
+    if(loading) return <Loading/>
+    
   return (
     <div>
       <Ads/>
-      <Products topic='Trending Products' rows={1} slidesPerView={7}/>
+      <div className='w-7xl m-auto'>
+      <Products topic='Trending Products' rows={1} slidesPerView={5}/>
+
+      </div>
     </div>
   )
 }
