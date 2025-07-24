74f90d435014fbd34b9298d0f4c624ecc01809b4
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-07-24 09:37:00 +0530

    Update Error404Page.jsx

--- src/Pages/Error404Page.jsx
+++ src/Pages/Error404Page.jsx
@@ -2,10 +2,14 @@ import error from '../assets/Images/404.webp'
 import Button from '@mui/material/Button';
 import HomeIcon from '@mui/icons-material/Home';
 import { useNavigate } from 'react-router-dom';
+import { useRouteError } from "react-router-dom";
+
 
 const Error404Page = () => {
 
       const navigate = useNavigate();
+        const error = useRouteError();
+
 
   return (
    <div className="flex w-screen h-screen gap-3 flex-col items-center justify-center">
@@ -14,6 +18,7 @@ const Error404Page = () => {
         Go to Home
       </Button>
   <img src={error} alt="404" style={{ width: "50%" }} />
+  <p>{error.statusText}</p>
 </div>
   )
 }
