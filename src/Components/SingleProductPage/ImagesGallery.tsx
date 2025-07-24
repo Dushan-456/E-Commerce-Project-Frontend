b48449ca99d74478a4b1afdf33c15f46aebaa097
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-07-24 14:29:01 +0530

    Create ImagesGallery.tsx

---
+++ src/Components/SingleProductPage/ImagesGallery.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ImagesGallery = () => {
+  return (
+    <div>
+      <img src="https://img.freepik.com/free-photo/bluetooth-speaker-digital-device_53876-96809.jpg?w=740" alt="" />
+    </div>
+  )
+}
+
+export default ImagesGallery
