4f0f6d85cbd38ddeaa61ce1055b5720b0f7f2058
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-07-24 15:57:33 +0530

    Update Product.jsx

--- src/Pages/Product/Product.jsx
+++ src/Pages/Product/Product.jsx
@@ -5,23 +5,23 @@ import "./Product.css";
 const Product = () => {
    return (
       <div className="w-7xl m-auto">
-         <div className="section1 flex ">
+         <div className="section1 mt-3 flex ">
             <div className="flex-3/4">
-               <div className=" flex">
+               <div className=" flex mb-5">
                   <div className="images flex-2/5">
-                     <h1>image</h1>
                      <ImagesGallery/>
                   </div>
                   <div className="flex-3/5 m-3">
                   <ProductDetails Price={25000} Discount={25}/>
                   </div>
                </div>
+               <hr />
                <div className="h-80">
                   <h1>reviews</h1>
                </div>
             </div>
-            <div className="bg-blue-600  flex-1/4">Right side
-            <div className="m-7 h-78 sticky top-35 bg-amber-50"><h1>buy</h1></div>
+            <div className="  flex-1/4">
+            <div className="m-7 h-78 sticky top-35 bg-blue-600"><h1>buy</h1></div>
             </div>
          </div>
       </div>
