858d4019460b29fbad716c68f465c7be4d942b77
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-07-27 13:35:19 +0530

    Update Error404Page.jsx

--- src/Pages/Error404Page.jsx
+++ src/Pages/Error404Page.jsx
@@ -1,26 +1,42 @@
-import error from '../assets/Images/404.webp'
-import Button from '@mui/material/Button';
-import HomeIcon from '@mui/icons-material/Home';
-import { useNavigate } from 'react-router-dom';
-import { useRouteError } from "react-router-dom";
-
+import error from "../assets/Images/404.webp";
+import Button from "@mui/material/Button";
+import HomeIcon from "@mui/icons-material/Home";
+import WarningAmberIcon from "@mui/icons-material/WarningAmber";
+import { useNavigate } from "react-router-dom";
+// import { useRouteError } from "react-router-dom";
 
 const Error404Page = () => {
+   const navigate = useNavigate();
+   //  const errormsg = useRouteError();
 
-      const navigate = useNavigate();
-        const errormsg = useRouteError();
+   return (
+      <div className="max-w-7xl mx-auto pt-10 text-center flex flex-col items-center pb-10 justify-center">
+         <WarningAmberIcon className="text-amber-500 !text-5xl" />
+         <h1 className="text-2xl font-semibold text-amber-500">
+            Page Not Found.
+         </h1>
+         <p className="text-gray-600 mt-2">
+            Sorry ! We couldn't find that page.
+         </p>
 
+         <img src={error} alt="404" style={{ width: "45%" }} />
+         <Button
+            variant="outlined"
+            onClick={() => navigate("/")}
+            startIcon={<HomeIcon />}>
+            Go to Home
+         </Button>
+         <br />
+         <p>
+            Please contact{" "}
+            <a href="" className="text-blue-500 font-bold">
+               System Administrator
+            </a>{" "}
+            if you believe this is an error in website.{" "}
+         </p>
+         {/* <p>{errormsg.statusText}</p> */}
+      </div>
+   );
+};
 
-  return (
-   <div className="flex w-screen h-screen gap-3 flex-col items-center justify-center">
-  <h1 className="text-4xl font-bold mt-4">Sorry !</h1>
-       <Button variant="outlined" onClick={() => navigate("/")} startIcon={<HomeIcon /> }>
-        Go to Home
-      </Button>
-  <img src={error} alt="404" style={{ width: "50%" }} />
-  <p>{errormsg.statusText}</p>
-</div>
-  )
-}
-
-export default Error404Page
+export default Error404Page;
