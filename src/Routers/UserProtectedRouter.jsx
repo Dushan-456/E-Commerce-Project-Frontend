7b0bb25bbfe069a91ed8ac8b6630c1815c663a28
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-07-27 13:35:27 +0530

    Delete UserProtectedRouter.jsx

--- src/Routers/UserProtectedRouter.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-
-const UserProtectedRouter = () => {
-   const user = true;
-   return user ? <Outlet /> : <Navigate to="/login" />;
-};
-
-export default UserProtectedRouter;
