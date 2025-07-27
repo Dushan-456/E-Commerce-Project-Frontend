903d33945fc37482e189e3f8f0db9281ed7e09b3
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-07-27 11:58:03 +0530

    Update CategoryItems.jsx

--- src/Pages/CategoryItems.jsx
+++ src/Pages/CategoryItems.jsx
@@ -4,6 +4,7 @@ const CategoryItems = () => {
   return (
     <div className='w-7xl m-auto pt-10'>
       <h1>Category Item</h1>
+      <img src="../assets/images/e-com_logo.png" alt="" />
     </div>
   )
 }
