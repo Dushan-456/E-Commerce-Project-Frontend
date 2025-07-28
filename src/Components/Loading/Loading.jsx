6bcd283766cbd83401b2a17d37050baeefea62bb
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-07-29 00:56:05 +0530

    Update Loading.jsx

--- src/Components/Loading/Loading.jsx
+++ src/Components/Loading/Loading.jsx
@@ -5,7 +5,7 @@ const Loading = () => {
     <div className='fixed top-0 left-0 gap-3 w-screen h-screen z-[9999] flex flex-col items-center justify-center bg-white/80 backdrop-blur-sm'>
       <div className="loading-blob"></div>
       <p>Loading..</p>
-      <div class="progress"></div>
+      <div className="progress"></div>
     </div>
   )
 }
