1f134455efdb0450578eef9962b99f30e713c20c
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-07-29 20:59:06 +0530

    Update ProductItem.jsx

--- src/Components/Products/ProductItem.jsx
+++ src/Components/Products/ProductItem.jsx
@@ -24,11 +24,9 @@ const ProductItem = ({
             </Link>
 
             <Link to="/add2cart">
-
                <button className="absolute top-2 right-2 bg-white/80 rounded-full p-2 z-49 hover:bg-black hover:text-white transition">
                   <AddShoppingCartIcon fontSize="small" />
                </button>
-
             </Link>
          </div>
 
@@ -38,8 +36,12 @@ const ProductItem = ({
             </p>
 
             <p className="text-sm mt-1">
-               <span className="text-black font-semibold">LKR {((Price * (100 - Discount)) / 100).toFixed(2)}</span>{" "}
-               <span className="text-gray-400 line-through text-xs ml-1">LKR {Price}.00</span>
+               <span className="text-black font-semibold">
+                  LKR {((Price * (100 - Discount)) / 100).toFixed(2)}
+               </span>{" "}
+               <span className="text-gray-400 line-through text-xs ml-1">
+                  LKR {Price}.00
+               </span>
                <span className="text-red-500 text-xs ml-2">-{Discount}%</span>
             </p>
 
@@ -52,7 +54,9 @@ const ProductItem = ({
                   size="small"
                />
                <span className="text-gray-600 text-xs ml-1">{ratings}</span>
-               <span className="text-gray-500 text-xs ml-2">| {itemSold}+ sold</span>
+               <span className="text-gray-500 text-xs ml-2">
+                  | {itemSold}+ sold
+               </span>
             </div>
          </Link>
       </div>
