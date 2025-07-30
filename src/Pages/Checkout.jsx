645c3b501d1f248b65eda51e05d5ea8f4d5144e1
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-07-30 11:30:21 +0530

    Create Checkout.jsx

---
+++ src/Pages/Checkout.jsx
@@ -0,0 +1,19 @@
+import CheckoutSummary from "../Components/Checkout/CheckoutSummary"
+
+const Checkout = () => {
+    return (
+        <div className='pt-10 w-7xl m-auto'>
+            <div className="flex">
+                <div className="flex-2/3">
+                    <h1>eeeeeeeeeeeeee</h1>
+                </div>
+                <div className="flex-1/3">
+                    <CheckoutSummary />
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Checkout
