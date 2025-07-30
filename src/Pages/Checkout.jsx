219425c66e4171aa0eca6e39ff67a5e19ff46aaa
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-07-30 15:47:02 +0530

    Update Checkout.jsx

--- src/Pages/Checkout.jsx
+++ src/Pages/Checkout.jsx
@@ -1,11 +1,12 @@
 import CheckoutSummary from "../Components/Checkout/CheckoutSummary"
+import ShippingAddress from "../Components/Checkout/ShippingAddress"
 
 const Checkout = () => {
     return (
-        <div className='pt-10 w-7xl m-auto'>
+        <div className='pt-10 w-7xl mb-10 m-auto'>
             <div className="flex">
-                <div className="flex-2/3">
-                    <h1>eeeeeeeeeeeeee</h1>
+                <div className="flex-2/3 mr-5">
+                    <ShippingAddress/>
                 </div>
                 <div className="flex-1/3">
                     <CheckoutSummary />
