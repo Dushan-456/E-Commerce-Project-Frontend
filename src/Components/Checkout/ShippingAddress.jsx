8475b3ef1cf5c6042227ac3866344db315689737
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-07-30 15:46:59 +0530

    Create ShippingAddress.jsx

---
+++ src/Components/Checkout/ShippingAddress.jsx
@@ -0,0 +1,11 @@
+
+const ShippingAddress = () => {
+    return (
+        <div className="flex flex-col gap-5 rounded-xl h-full p-5"
+            style={{ boxShadow: "rgba(0, 0, 0, 0.1) 0px 4px 12px" }}>
+                <h1>hellooooooooooo</h1>
+        </div>
+    )
+}
+
+export default ShippingAddress
