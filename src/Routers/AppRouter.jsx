d4b7cd0856e5d6de7312b9e62b8b76774af50a5e
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-07-30 11:30:23 +0530

    Update AppRouter.jsx

--- src/Routers/AppRouter.jsx
+++ src/Routers/AppRouter.jsx
@@ -1,9 +1,9 @@
 import {
-   BrowserRouter,
-   createBrowserRouter,
-   Route,
-   RouterProvider,
-   Routes,
+    BrowserRouter,
+    createBrowserRouter,
+    Route,
+    RouterProvider,
+    Routes,
 } from "react-router-dom";
 import MainLayout from "../Layout/MainLayout";
 import Home from "../Pages/Home";
@@ -14,68 +14,72 @@ import Error404Page from "../Pages/Error404Page";
 import CategoryItems from "../Pages/CategoryItems";
 import SingleProductPage from "../Pages/Product/SingleProductPage";
 import ProtectedRouter from "./ProtectedRouter";
+import Checkout from "../Pages/Checkout";
 
 const router = createBrowserRouter([
-   {
-      path: "/",
-      element: <MainLayout />,
-      // errorElement: <Error404Page />,
-      children: [
-         {
-            index: true,
-            element: <Home />,
-         },
-         {
-            path: "about",
-            element: <About />,
-         },
-         {
-            path: "category/:CategoryID",
-            element: <CategoryItems />,
-         },
-         {
-            path: "product/:ProductID",
-            element: <SingleProductPage />,
-         },
-         {
-            element: <ProtectedRouter ProtectedRole="user"/>,
-            children: [
-               {
-                  path: "profile",
-                  element: <UserProfile />,
-               },
-            ],
-         },
-         {
-            path: "*",
-            element: <Error404Page />,
-         },
-      ],
-   },
-   {
-      path: "/login",
-      element: <LoginPage />,
-   },
+    {
+        path: "/",
+        element: <MainLayout />,
+        // errorElement: <Error404Page />,
+        children: [
+            {
+                index: true,
+                element: <Home />,
+            },
+            {
+                path: "about",
+                element: <About />,
+            },
+            {
+                path: "category/:CategoryID",
+                element: <CategoryItems />,
+            },
+            {
+                path: "product/:ProductID",
+                element: <SingleProductPage />,
+            }, {
+                path: "checkout",
+                element: <Checkout />,
+            },
+            {
+                element: <ProtectedRouter ProtectedRole="user" />,
+                children: [
+                    {
+                        path: "profile",
+                        element: <UserProfile />,
+                    },
+                ],
+            },
+            {
+                path: "*",
+                element: <Error404Page />,
+            },
+        ],
+    },
+    {
+        path: "/login",
+        element: <LoginPage />,
+    },
 ]);
 
 const AppRouter = () => {
-   return (
-      <RouterProvider router={router} />
+    return (
+        <RouterProvider router={router} />
 
-      // <BrowserRouter>
-      // <Routes>
-      //     <Route path="/" element={<MainLayout/>}>
-      //         <Route index element={<Home/>}/>
-      //         <Route path="about" element={<About/>}/>
-      //         <Route element={<UserProtectedRouter/>}>
-      //             <Route path="profile" element={<UserProfile/>}/>
-      //         </Route>
-      //     </Route>
-      //         <Route path="/login" element={<LoginPage/>}/>
-      // </Routes>
+        // <BrowserRouter>
+        // <Routes>
+        //     <Route path="/" element={<MainLayout/>}>
+        //         <Route index element={<Home/>}/>
+        //         <Route path="about" element={<About/>}/>
+        //         <Route element={<UserProtectedRouter/>}>
+        //             <Route path="profile" element={<UserProfile/>}/>
+        //         </Route>
+        //     </Route>
+        //         <Route path="/login" element={<LoginPage/>}/>
+        // </Routes>
 
-      // </BrowserRouter>
-   );
+        // </BrowserRouter>
+    );
 };
 
 export default AppRouter;
